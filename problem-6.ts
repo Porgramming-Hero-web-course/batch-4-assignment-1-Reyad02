b3e53016201f87db1637983db431de38f7341ba9
unknown <areyad203011@bscse.uiu.ac.bd>
2024-11-07 21:50:28 +0600

    6th problem updated

--- problem-6.ts
+++ problem-6.ts
@@ -20,7 +20,6 @@
         return profile
     }
 
-    // Sample Input :
     const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
     console.log(updateProfile(myProfile, { age: 26 }));
 
